464755d0d19d9daa44ded53d038c9fa8e9e7dc59
OneShot119 <dlang119@gmail.com>
2015-05-28 18:28:07 -0400

    sibyl fixes

--- c13720039.lua
+++ c13720039.lua
@@ -4,15 +4,14 @@ function c13720039.initial_effect(c)
 	e1:SetDescription(aux.Stringid(13720039,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_FLIP+EFFECT_TYPE_TRIGGER_O)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
-	e1:SetCountLimit(1,13720039)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
 	e1:SetTarget(c13720039.target)
 	e1:SetOperation(c13720039.operation)
 	c:RegisterEffect(e1)
 
 end
 function c13720039.filter(c,e,tp)
-	return c:IsType(TYPE_FLIP)
+	return c:IsType(TYPE_FLIP) and c:IsLevelAbove(3) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEDOWN)
 end
 function c13720039.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_DECK+LOCATION_HAND) and chkc:IsControler(tp) and c13720039.filter(chkc,e,tp) end
