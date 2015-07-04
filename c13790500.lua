995114bfa2326077a8103e2ba7709181834772d4
OneShot119 <dlang119@gmail.com>
2015-07-04 00:51:19 -0400

    Come on, shake your body baby, do the conga
    I know you can't control yourself any longer

--- c13790500.lua
+++ c13790500.lua
@@ -1,25 +1,39 @@
----Red-Eyes Black Flare Dragon
+---sky dragoons
 function c13790500.initial_effect(c)
-	aux.EnableDualAttribute(c)
+	--pendulum summon
+	aux.AddPendulumProcedure(c)
+	--Activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(13790500,0))
-	e1:SetCategory(CATEGORY_DESTROY)
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e1:SetCode(EVENT_PHASE+PHASE_BATTLE)
-	e1:SetRange(LOCATION_MZONE)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e1:SetCountLimit(1,13790500)
-	e1:SetCondition(aux.IsDualState)
-	e1:SetTarget(c13790500.destg)
-	e1:SetOperation(c13790500.desop)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e1)
+	--Destroy
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_DESTROY)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_BATTLE_DAMAGE)
+	e2:SetRange(LOCATION_PZONE)
+	e2:SetCountLimit(1)
+	e2:SetCondition(c13790500.decon)
+	e2:SetTarget(c13790500.detg)
+	e2:SetOperation(c13790500.deop)
+	c:RegisterEffect(e2)
 end
-function c13790500.destg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():GetBattledGroupCount()>0 end
+function c13790500.decon(e,tp,eg,ep,ev,re,r,rp)
+	if ep==tp then return false end
+	local rc=eg:GetFirst()
+	return rc:IsControler(tp) and rc:IsType(TYPE_NORMAL)
 end
-function c13790500.desop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if c:IsRelateToEffect(e) then
-		Duel.Damage(1-tp,e:GetHandler():GetBaseAttack(),REASON_EFFECT)
+function c13790500.detg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsOnField() and chkc:IsDestructable() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsDestructable,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(tp,Card.IsDestructable,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+end
+function c13790500.deop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) then
+		Duel.Destroy(tc,REASON_EFFECT)
 	end
 end
