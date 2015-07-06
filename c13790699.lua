cc1a7309625fa02e295ad3be592c4f479553551d
OneShot119 <dlang119@gmail.com>
2015-07-06 14:40:36 -0400

    hack cannot read confirmed - Raid Raptor Devil

--- c13790699.lua
+++ c13790699.lua
@@ -5,7 +5,7 @@ function c13790699.initial_effect(c)
 	c:EnableReviveLimit()
 	--attack up
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_DAMAGE)
+	e1:SetCategory(CATEGORY_DAMAGE)
 	e1:SetDescription(aux.Stringid(88120966,0))
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
@@ -26,24 +26,17 @@ function c13790699.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
 end
 function c13790699.filter(c)
-	return c:IsDestructable() and c:IsFaceup() and bit.band(c:GetSummonType(),SUMMON_TYPE_SPECIAL)==SUMMON_TYPE_SPECIAL
+	return c:IsFaceup() and bit.band(c:GetSummonType(),SUMMON_TYPE_SPECIAL)==SUMMON_TYPE_SPECIAL
 end
 function c13790699.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and c13790699.filter(chkc) end
 	if chk==0 then return Duel.IsExistingTarget(c13790699.filter,tp,0,LOCATION_MZONE,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
 	local g=Duel.SelectTarget(tp,c13790699.filter,tp,0,LOCATION_MZONE,1,1,nil)
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,0)
 end
 function c13790699.operation(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) then
-		if Duel.Destroy(tc,REASON_EFFECT)~=0 then
-			Duel.BreakEffect()
-			local atk=tc:GetBaseAttack()
-			if atk<0 then atk=0 end
-			Duel.Damage(1-tp,atk,REASON_EFFECT)
-		end
+	if tc:IsFaceup() and tc:IsRelateToEffect(e) then
+		Duel.Damage(1-tp,tc:GetBaseAttack(),REASON_EFFECT)
 	end
 end
-
