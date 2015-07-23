f320567061b7f80e8df1d74f520d2a616ea98550
StormWulf <auronzeroo@aim.com>
2015-07-23 03:31:13 -0400

    Update c13790633.lua
    Fixed to make it only trigger if controlled by the original owner

--- c13790633.lua
+++ c13790633.lua
@@ -13,7 +13,7 @@ function c13790633.initial_effect(c)
 end
 function c13790633.eqcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	return c:IsPreviousLocation(LOCATION_MZONE) and c:IsReason(REASON_DESTROY) and
+	return c:GetPreviousControler()==tp and c:IsPreviousLocation(LOCATION_MZONE) and c:IsReason(REASON_DESTROY) and
 	 (c:IsReason(REASON_BATTLE) or c:IsReason(REASON_EFFECT) and re:IsActiveType(TYPE_SPELL))
 end
 function c13790633.eqtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
