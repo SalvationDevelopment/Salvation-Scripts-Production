2fa91b5aa3d0e7c76f1f0a3a1c54ef25799c72d1
OneShot119 <dlang119@gmail.com>
2015-07-25 23:07:59 -0400

    painful decision

--- c13790665.lua
+++ c13790665.lua
@@ -2,10 +2,10 @@
 function c13790665.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetCategory(CATEGORY_TOGRAVE+CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetCountLimit(1,13790665)
+	e1:SetCountLimit(1,13790665+EFFECT_COUNT_CODE_OATH)
 	e1:SetTarget(c13790665.target)
 	e1:SetOperation(c13790665.activate)
 	c:RegisterEffect(e1)
@@ -22,17 +22,16 @@ function c13790665.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK)
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
-
-function c13790665.activate(e,tp,eg,ep,ev,re,r,rp)
+function c13790665.operation(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local g=Duel.SelectMatchingCard(tp,c13790665.filter,tp,LOCATION_DECK,0,1,1,nil)
-	if g:GetCount()>0 and Duel.SendtoGrave(g,REASON_EFFECT)>0 then
-		Duel.BreakEffect()
-		local tc=g:GetFirst()
-		local hc=Duel.GetFirstMatchingCard(c13790665.filter2,tp,LOCATION_DECK,0,nil,tc:GetCode())
-		if hc then
-			Duel.SendtoHand(hc,nil,REASON_EFFECT)
-			Duel.ConfirmCards(1-tp,hc)
+	local g1=Duel.SelectMatchingCard(tp,c13790665.filter,tp,LOCATION_DECK,0,1,1,nil,tp)
+	if g1:GetCount()>0 then
+		local tg1=g1:GetFirst()
+		Duel.SendtoGrave(tg1,REASON_EFFECT)
+		local g2=Duel.SelectMatchingCard(tp,c13790665.filter2,tp,LOCATION_DECK,0,1,1,tg1,tg1:GetCode())
+		if g2:GetCount()>0 then
+			Duel.SendtoHand(g2,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,g2)
 		end
 	end
 end
