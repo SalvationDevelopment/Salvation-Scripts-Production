ba787d8f2fd1955d8a30e32341a863024f58fc25
OneShot119 <dlang119@gmail.com>
2015-08-12 07:23:44 -0400

    High-speed riders
    All credit soley to OneShot

---
+++ c6408.lua
@@ -0,0 +1,70 @@
+--Mirror Resonator
+function c6408.initial_effect(c)
+	--special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(86585274,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_HAND+LOCATION_GRAVE)
+	e1:SetCountLimit(1,6408)
+	e1:SetCondition(c6408.spcon)
+	e1:SetTarget(c6408.sptg)
+	e1:SetOperation(c6408.spop)
+	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(28150174,0))
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetCountLimit(1)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTarget(c6408.target)
+	e2:SetOperation(c6408.operation)
+	c:RegisterEffect(e2)
+end
+function c6408.cfilter(c)
+	return c:GetSummonLocation()==LOCATION_EXTRA
+end
+function c6408.spcon(e,c)
+	if c==nil then return true end
+	return Duel.IsExistingMatchingCard(c6408.cfilter,tp,0,LOCATION_MZONE,1,nil)
+	and not Duel.IsExistingMatchingCard(c6408.cfilter,tp,LOCATION_MZONE,0,1,nil)
+end
+function c6408.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function c6408.spop(e,tp,eg,ep,ev,re,r,rp,c)
+	local c=e:GetHandler()
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	if c:IsRelateToEffect(e) then
+		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetReset(RESET_EVENT+0xfe0000)
+		e1:SetValue(LOCATION_REMOVED)
+		c:RegisterEffect(e1)
+	end
+end
+function c6408.filter(c)
+	return c:IsFaceup() and c:GetLevel()>0
+end
+function c6408.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and c6408.filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c6408.filter,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	local g=Duel.SelectTarget(tp,c6408.filter,tp,0,LOCATION_MZONE,1,1,nil)
+end
+function c6408.operation(e,tp,eg,ep,ev,re,r,rp,c)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	local lv=tc:GetLevel()
+	--synchro level
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_SYNCHRO_LEVEL)
+	e1:SetValue(lv)
+	c:RegisterEffect(e1)
+end
