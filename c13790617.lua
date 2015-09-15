75949f54336e50a446fb67e5056a21164186034a
StormWulf <auronzeroo@aim.com>
2015-09-15 14:05:48 -0400

    BOSH batch

--- c13790617.lua
+++ c13790617.lua
@@ -1,117 +1,59 @@
---The Legendary Fisherman III
+--Buster Blader, the Dragon Destroyer
 function c13790617.initial_effect(c)
+	--fusion material
 	c:EnableReviveLimit()
-	--cannot special summon
+	aux.AddFusionProcCodeFun(c,78193831,aux.FilterBoolFunction(Card.IsRace,RACE_DRAGON),1,false,false)
+	--cannot direct attack
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e1:SetCode(EFFECT_CANNOT_DIRECT_ATTACK)
 	c:RegisterEffect(e1)
-	--special summon
+	--ATK Gain
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD)
-	e2:SetCode(EFFECT_SPSUMMON_PROC)
-	e2:SetProperty(EFFECT_FLAG_UNCOPYABLE)
-	e2:SetRange(LOCATION_HAND)
-	e2:SetCondition(c13790617.spcon)
-	e2:SetOperation(c13790617.spop)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetValue(c13790617.val)
 	c:RegisterEffect(e2)
-	--destroy
-	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(13790617,0))
-	e3:SetCategory(CATEGORY_DESTROY)
-	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e3:SetCost(c13790617.damcost)
-	e3:SetTarget(c13790617.destg)
-	e3:SetOperation(c13790617.desop)
+	local e3=e2:Clone()
+	e3:SetCode(EFFECT_UPDATE_DEFENCE)
 	c:RegisterEffect(e3)
-	--reduce damage
+	--spsummon condition
 	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(13790617,1))
-	e4:SetType(EFFECT_TYPE_IGNITION)
-	e4:SetRange(LOCATION_MZONE)
-	e4:SetTarget(c13790617.target)
-	e4:SetOperation(c13790617.operation)
+	e4:SetType(EFFECT_TYPE_SINGLE)
+	e4:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e4:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e4:SetValue(aux.fuslimit)
 	c:RegisterEffect(e4)
-	--indes
+	--Pos Change
 	local e5=Effect.CreateEffect(c)
-	e5:SetType(EFFECT_TYPE_SINGLE)
-	e5:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e5:SetType(EFFECT_TYPE_FIELD)
+	e5:SetCode(EFFECT_SET_POSITION)
 	e5:SetRange(LOCATION_MZONE)
-	e5:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
-	e5:SetValue(1)
+	e5:SetTarget(c13790617.target)
+	e5:SetTargetRange(0,LOCATION_MZONE)
+	e5:SetValue(POS_FACEUP_DEFENCE)
 	c:RegisterEffect(e5)
-	local e6=e5:Clone()
-	e6:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	local e6=Effect.CreateEffect(c)
+	e6:SetType(EFFECT_TYPE_FIELD)
+	e6:SetCode(EFFECT_CANNOT_TRIGGER)
+	e6:SetTargetRange(0,LOCATION_MZONE)
+	e6:SetRange(LOCATION_MZONE)
+	e6:SetTarget(c13790617.target)
 	c:RegisterEffect(e6)
-	--immune
+	--pierce
 	local e7=Effect.CreateEffect(c)
 	e7:SetType(EFFECT_TYPE_SINGLE)
-	e7:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e7:SetRange(LOCATION_MZONE)
-	e7:SetCode(EFFECT_IMMUNE_EFFECT)
-	e7:SetValue(c13790617.efilter)
+	e7:SetCode(EFFECT_PIERCE)
 	c:RegisterEffect(e7)
 end
-function c13790617.spcon(e,c)
-	if c==nil then return true end
-	return Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>-1
-		and Duel.CheckReleaseGroup(c:GetControler(),Card.IsCode,1,nil,3643300)
+function c13790617.val(e,c)
+	return Duel.GetMatchingGroupCount(c78193831.filter,c:GetControler(),0,LOCATION_GRAVE+LOCATION_MZONE,nil)*1000
 end
-function c13790617.spop(e,tp,eg,ep,ev,re,r,rp,c)
-	local g=Duel.SelectReleaseGroup(c:GetControler(),Card.IsCode,1,1,nil,3643300)
-	Duel.Release(g,REASON_COST)
+function c13790617.filter(c)
+	return c:IsRace(RACE_DRAGON) and (c:IsLocation(LOCATION_GRAVE) or c:IsFaceup())
 end
-
-function c13790617.damcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():GetAttackAnnouncedCount()==0 end
-	local e1=Effect.CreateEffect(e:GetHandler())
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_CANNOT_ATTACK)
-	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_OATH)
-	e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
-	e:GetHandler():RegisterEffect(e1)
-end
-function c13790617.destg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToRemove,tp,0,LOCATION_MZONE,1,nil) end
-	local g=Duel.GetMatchingGroup(Card.IsAbleToRemove,tp,0,LOCATION_MZONE,nil)
-	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,g:GetCount(),0,0)
-end
-function c13790617.desop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(Card.IsAbleToRemove,tp,0,LOCATION_MZONE,nil)
-	if g:GetCount()>0 then
-		Duel.Remove(g,POS_FACEUP,REASON_EFFECT)
-	end
-end
-
-function c13790617.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_REMOVED) end
-	if chk==0 then return Duel.IsExistingTarget(Card.IsAbleToGrave,tp,0,LOCATION_REMOVED,1,nil) end
-	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,nil,0,0)
-end
-function c13790617.operation(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetFieldGroup(tp,0,LOCATION_REMOVED)
-	Duel.SendtoGrave(g,REASON_EFFECT+REASON_RETURN)
-	
-	local e1=Effect.CreateEffect(e:GetHandler())
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_CHANGE_DAMAGE)
-	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e1:SetTargetRange(0,1)
-	e1:SetValue(c13790617.damval)
-	e1:SetReset(RESET_PHASE+PHASE_END,1)
-	Duel.RegisterEffect(e1,tp)
-	Duel.RegisterFlagEffect(tp,13790617,RESET_PHASE+PHASE_END,0,1)
-end
-function c13790617.damval(e,re,dam,r,rp,rc)
-	local tp=e:GetHandlerPlayer()
-	if Duel.GetFlagEffect(tp,13790617)==0 then return dam end
-	if Duel.GetFlagEffect(tp,13790617)==1 or bit.band(r,REASON_BATTLE+REASON_EFFECT)~=0 then 
-	Duel.ResetFlagEffect(tp,13790617)
-	return dam*2 end
-end
-
-function c13790617.efilter(e,te)
-	return te:IsActiveType(TYPE_SPELL) or te:IsActiveType(TYPE_TRAP)
+function c13790617.target(e,c)
+	return c:IsRace(RACE_DRAGON)
 end
