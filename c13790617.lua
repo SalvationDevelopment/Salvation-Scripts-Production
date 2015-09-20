c14702a4313515ae47633bee40a5a997154a497e
OneShot119 <dlang119@gmail.com>
2015-09-19 20:17:27 -0400

    s

--- c13790617.lua
+++ c13790617.lua
@@ -3,7 +3,7 @@
 function c13790617.initial_effect(c)
 	--fusion material
 	c:EnableReviveLimit()
-	aux.AddFusionProcCodeFun(c,78193831,aux.FilterBoolFunction(Card.IsRace,RACE_DRAGON),1,false,false)
+	aux.AddFusionProcCodeFun(c,78193831,aux.FilterBoolFunction(Card.IsRace,RACE_DRAGON),1,true,true)
 	--cannot direct attack
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
