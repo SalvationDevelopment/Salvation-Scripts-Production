47aa4a099c6cec67dc7a2a42d38ec33b032d6c1a
StormWulf <auronzeroo@aim.com>
2015-10-03 21:02:14 -0400

    Supercell and Toon BB

--- c13790636.lua
+++ c13790636.lua
@@ -1,85 +1,110 @@
---Skilled Red Magician
+--Majespecter Supercell
 function c13790636.initial_effect(c)
-	c:EnableCounterPermit(0x3001)
-	c:SetCounterLimit(0x3001,3)
-	--add counter
-	local e0=Effect.CreateEffect(c)
-	e0:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
-	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-	e0:SetCode(EVENT_CHAINING)
-	e0:SetRange(LOCATION_MZONE)
-	e0:SetOperation(aux.chainreg)
-	c:RegisterEffect(e0)
+	--Activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e1:SetCode(EVENT_CHAIN_SOLVED)
-	e1:SetRange(LOCATION_MZONE)
-	e1:SetOperation(c13790636.acop)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(c13790636.target1)
+	e1:SetOperation(c13790636.drop)
+	e1:SetHintTiming(0,0x1c0)
 	c:RegisterEffect(e1)
-	--special summon
+	--Draw
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(13790636,0))
-	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e2:SetType(EFFECT_TYPE_IGNITION)
-	e2:SetRange(LOCATION_MZONE)
-	e2:SetCost(c13790636.spcost)
-	e2:SetTarget(c13790636.sptg)
-	e2:SetOperation(c13790636.spop)
+	e2:SetCategory(CATEGORY_TODECK+CATEGORY_DRAW)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCountLimit(1)
+	e2:SetTarget(c13790636.drtg)
+	e2:SetOperation(c13790636.drop)
 	c:RegisterEffect(e2)
-	--counter
+	--cannot disable
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(13790636,1))
-	e3:SetType(EFFECT_TYPE_IGNITION)
-	e3:SetRange(LOCATION_GRAVE)
-	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e3:SetCost(c13790636.ctcost)
-	e3:SetTarget(c13790636.cttg)
-	e3:SetOperation(c13790636.ctop)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_CANNOT_DISABLE)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetTargetRange(LOCATION_SZONE,0)
+	e3:SetCondition(c13790636.tgcon)
+	e3:SetValue(c13790636.effectfilter)
 	c:RegisterEffect(e3)
+	--inactivatable
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_FIELD)
+	e4:SetCode(EFFECT_CANNOT_INACTIVATE)
+	e4:SetRange(LOCATION_SZONE)
+	e4:SetCondition(c13790636.tgcon)
+	e4:SetValue(c13790636.effectfilter)
+	c:RegisterEffect(e4)
+	local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_FIELD)
+	e5:SetCode(EFFECT_CANNOT_DISEFFECT)
+	e5:SetRange(LOCATION_SZONE)
+	e5:SetCondition(c13790636.tgcon)
+	e5:SetValue(c13790636.effectfilter)
+	c:RegisterEffect(e5)
+
 end
-function c13790636.acop(e,tp,eg,ep,ev,re,r,rp)
-	if re:IsHasType(EFFECT_TYPE_ACTIVATE) and re:IsActiveType(TYPE_SPELL) and e:GetHandler():GetFlagEffect(1)>0 then
-		e:GetHandler():AddCounter(0x3001,1)
+
+function c13790636.filter(c)
+	return c:IsSetCard(0xd0) and c:IsAbleToDeck()
+end
+function c13790636.target1(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	if e:GetHandler():GetFlagEffect(tp,13790636)==0
+		and Duel.IsPlayerCanDraw(tp,1)
+		and Duel.IsExistingMatchingCard(Card.IsSetCard,tp,LOCATION_GRAVE,0,5,nil,0xd0)
+		and Duel.SelectYesNo(tp,aux.Stringid(13790636,0)) then
+		e:SetCategory(CATEGORY_TODECK+CATEGORY_DRAW)
+		e:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+		e:GetHandler():RegisterFlagEffect(tp,RESET_PHASE+PHASE_END,0,1)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+		local g=Duel.SelectTarget(tp,c13790636.filter,tp,LOCATION_GRAVE,0,5,5,nil)
+		Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,1,tp,LOCATION_HAND)
+		Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+		e:SetLabel(1)
+		e:GetHandler():RegisterFlagEffect(0,RESET_CHAIN,EFFECT_FLAG_CLIENT_HINT,1,0,aux.Stringid(13790636,2))
+	else
+		e:SetCategory(0)
+		e:SetProperty(0)
+		e:SetLabel(0)
 	end
 end
-function c13790636.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():GetCounter(0x3001)==3 and e:GetHandler():IsReleasable() end
-	Duel.Release(e:GetHandler(),REASON_COST)
+function c13790636.drtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:GetLocation()==LOCATION_GRAVE and chkc:GetControler()==tp and c13790636.filter(chkc) end
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) and e:GetHandler():GetFlagEffect(tp,13790636)==0
+		and Duel.IsExistingTarget(c13790636.filter,tp,LOCATION_GRAVE,0,5,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local g=Duel.SelectTarget(tp,c13790636.filter,tp,LOCATION_GRAVE,0,5,5,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,g:GetCount(),0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
 end
-function c13790636.filter(c,e,tp)
-	return c:IsSetCard(0x45) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-		and not c:IsHasEffect(EFFECT_NECRO_VALLEY)
+function c13790636.tgfilter(c,e)
+	return not c:IsRelateToEffect(e)
 end
-function c13790636.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>-1
-		and Duel.IsExistingMatchingCard(c13790636.filter,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE,0,1,nil,e,tp) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE)
-end
-function c13790636.spop(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,c13790636.filter,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil,e,tp)
-	if g:GetCount()>0 then
-		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+function c13790636.drop(e,tp,eg,ep,ev,re,r,rp)
+	local tg=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+	if not tg or tg:FilterCount(Card.IsRelateToEffect,nil,e)~=5 then return end
+	Duel.SendtoDeck(tg,nil,0,REASON_EFFECT)
+	local g=Duel.GetOperatedGroup()
+	local ct=g:FilterCount(Card.IsLocation,nil,LOCATION_DECK+LOCATION_EXTRA)
+	if ct==2 then
+		Duel.ShuffleDeck(tp)
+		Duel.BreakEffect()
+		Duel.Draw(tp,1,REASON_EFFECT)
 	end
 end
-function c13790636.ctcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():IsAbleToRemoveAsCost() end
-	Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_COST)
+
+
+function c13790636.tgcon(e,tp,eg,ep,ev,re,r,rp)
+	local tc1=Duel.GetFieldCard(tp,LOCATION_SZONE,6)
+	local tc2=Duel.GetFieldCard(tp,LOCATION_SZONE,7)
+	return tc1:IsSetCard(0xd0) or tc2:IsSetCard(0xd0)
 end
-function c13790636.ctfilter(c)
-	return c:IsFaceup() and c:IsCanAddCounter(0x3001,1)
-end
-function c13790636.cttg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsOnField() and chkc:IsControler(tp) and c13790636.ctfilter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(c13790636.ctfilter,tp,LOCATION_ONFIELD,0,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(13790636,2))
-	Duel.SelectTarget(tp,c13790636.ctfilter,tp,LOCATION_ONFIELD,0,1,1,nil)
-	Duel.SetOperationInfo(0,CATEGORY_COUNTER,nil,1,0,0x3001)
-end
-function c13790636.ctop(e,tp,eg,ep,ev,re,r,rp)
-	local tc=Duel.GetFirstTarget()
-	if tc:IsFaceup() and tc:IsRelateToEffect(e) then
-		tc:AddCounter(0x3001,1)
-	end
+function c13790636.effectfilter(e,ct)
+	local p=e:GetHandler():GetControler()
+	local te,tp=Duel.GetChainInfo(ct,CHAININFO_TRIGGERING_EFFECT,CHAININFO_TRIGGERING_PLAYER)
+	local tc=te:GetHandler()
+	return p==tp and tc:IsType(TYPE_SPELL+TYPE_TRAP) and tc:IsSetCard(0xd0) and tc~=e:GetHandler()
 end
