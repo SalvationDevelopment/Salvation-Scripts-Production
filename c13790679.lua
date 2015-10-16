c0f5067708b1cb6826427da438a12d2aebf7f66e
StormWulf <auronzeroo@aim.com>
2015-10-16 14:08:46 -0400

    Hack did fixes and added False Accusations
    Cards edited:
    Dynaster
    Superheavy Maju Kyu-B
    Shiranui Style: Reincarnation Formation

--- c13790679.lua
+++ c13790679.lua
@@ -1,30 +1,42 @@
---Great Horn of Heaven
+--False Accusations
 function c13790679.initial_effect(c)
+	--destroy
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_DISABLE_SUMMON+CATEGORY_DESTROY)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_SPSUMMON)
-	e1:SetCondition(c13790679.condition1)
-	e1:SetTarget(c13790679.target1)
-	e1:SetOperation(c13790679.activate1)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(TIMING_SPSUMMON)
+	e1:SetCondition(c13790679.condition)
+	e1:SetCountLimit(1,13790679)
+	e1:SetTarget(c13790679.target)
+	e1:SetOperation(c13790679.operation)
 	c:RegisterEffect(e1)
 end
-function c13790679.condition1(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetCurrentChain()==0 and Duel.GetTurnPlayer()~=tp and
-	(Duel.GetCurrentPhase()==PHASE_MAIN1 or Duel.GetCurrentPhase()==PHASE_MAIN2)
+function c13790679.condition(e,tp,eg,ep,ev,re,r,rp)
+	local ct1=Duel.GetFieldGroupCount(tp,LOCATION_ONFIELD+LOCATION_HAND,0)
+	local ct2=Duel.GetFieldGroupCount(tp,0,LOCATION_ONFIELD+LOCATION_HAND)
+	return ct1<ct2
 end
-function c13790679.target1(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
-	Duel.SetOperationInfo(0,CATEGORY_DISABLE_SUMMON,eg,eg:GetCount(),0,0)
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,eg:GetCount(),0,0)
+function c13790679.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	e:SetLabel(1)
+	if chkc then return chkc:IsOnField() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,0,LOCATION_ONFIELD,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(tp,Card.IsFaceup,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+	e:SetLabel(g:GetFirst():GetCode())
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
 end
-function c13790679.activate1(e,tp,eg,ep,ev,re,r,rp)
-	Duel.NegateSummon(eg)
-	Duel.Destroy(eg,REASON_EFFECT)
-	Duel.Draw(1-tp,1,REASON_EFFECT)
-	if Duel.GetCurrentPhase()==PHASE_MAIN1 then
-		Duel.SkipPhase(1-tp,PHASE_MAIN1,RESET_PHASE+PHASE_MAIN1,1)
-	else
-		Duel.SkipPhase(1-tp,PHASE_MAIN2,RESET_PHASE+PHASE_MAIN2,1)
-	end
+function c13790679.activate(e,tp,eg,ep,ev,re,r,rp)
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetCode(EFFECT_CANNOT_ACTIVATE)
+	e1:SetTargetRange(1,1)
+	e1:SetValue(c13790679.aclimit)
+	e1:SetLabel(e:GetLabel())
+	Duel.RegisterEffect(e1,tp)
 end
+function c13790679.aclimit(e,re,tp)
+	return re:GetHandler():IsCode(e:GetLabel()) and (not re:GetHandler():IsImmuneToEffect(e) or not re:GetHandler()==e:GetHandler())
+end
+
