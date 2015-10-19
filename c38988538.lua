28caf21e7dbeed41549339c44beb26052928ce30
Ragna_Edge <j.yarel.g.aponte@gmail.com>
2015-10-19 19:26:12 -0400

    Fixed Everything

--- c38988538.lua
+++ c38988538.lua
@@ -1,4 +1,5 @@
 --Dinomist Plesios
+--Fixed by Ragna_Edge
 function c38988538.initial_effect(c)
 	--pendulum summon
 	aux.AddPendulumProcedure(c)
@@ -10,48 +11,39 @@ function c38988538.initial_effect(c)
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(38988538,0))
 	e2:SetCategory(CATEGORY_DISABLE)
-	e2:SetType(EFFECT_TYPE_QUICK_O)
 	e2:SetProperty(EFFECT_FLAG_NO_TURN_RESET)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e2:SetCode(EVENT_CHAINING)
 	e2:SetRange(LOCATION_PZONE)
-	e2:SetCountLimit(1)
-	e2:SetCondition(c38988538.negcon)
-	e2:SetTarget(c38988538.negtg)
 	e2:SetOperation(c38988538.negop)
 	c:RegisterEffect(e2)
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_FIELD)
-	e3:SetCode(EFFECT_UPDATE_ATTACK)
 	e3:SetRange(LOCATION_MZONE)
 	e3:SetTargetRange(0,LOCATION_MZONE)
-	e3:SetValue(c38988538.atkval)
+	e3:SetCode(EFFECT_UPDATE_ATTACK)
+	e3:SetValue(c38988538.val)
 	c:RegisterEffect(e3)
-	local e4=Effect.CreateEffect(c)
-	e4:SetType(EFFECT_TYPE_FIELD)
+	local e4=e3:Clone()
 	e4:SetCode(EFFECT_UPDATE_DEFENCE)
-	e4:SetRange(LOCATION_MZONE)
-	e4:SetTargetRange(0,LOCATION_MZONE)
-	e4:SetValue(c38988538.atkval)
 	c:RegisterEffect(e4)
 end
-function c38988538.tfilter(c)
-	return c:IsFaceup() and c:IsSetCard(0x1e71) and c:IsControler(tp)
-end
-function c38988538.negcon(e,tp,eg,ep,ev,re,r,rp)
-	if not re:IsHasProperty(EFFECT_FLAG_CARD_TARGET) then return end
-	if not re:IsHasType(EFFECT_TYPE_ACTIVATE) then return false end
-	local g=Duel.GetChainInfo(ev,CHAININFO_TARGET_CARDS)
-	return g and g:IsExists(c38988538.tfilter,1,nil)and g:GetFirst()~=e:GetHandler() and Duel.IsChainDisablable(ev)
-end
-function c38988538.negtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
-	Duel.SetOperationInfo(0,CATEGORY_DISABLE,eg,1,0,0)
+function c38988538.tfilter(c,tp)
+	return c:IsLocation(LOCATION_ONFIELD) and c:IsControler(tp) and c:IsFaceup() and c:IsSetCard(0x1e71)
 end
 function c38988538.negop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.NegateEffect(ev)
-	Duel.Destroy(e:GetHandler(),REASON_EFFECT)
+	if ep==tp then return end
+	if not re:IsHasProperty(EFFECT_FLAG_CARD_TARGET) then return end
+	local g=Duel.GetChainInfo(ev,CHAININFO_TARGET_CARDS)
+	if g and g:IsExists(c38988538.tfilter,1,nil,tp) and g~=e:GetHandler() and Duel.IsChainDisablable(ev) 
+		and Duel.SelectYesNo(tp,aux.Stringid(38988538,0))then
+		Duel.NegateEffect(ev)
+		Duel.Destroy(e:GetHandler(),REASON_EFFECT)
+	end
 end
-
-function c38988538.atkval(e,c)
-	return Duel.GetMatchingGroupCount(Card.IsSetCard,tp,LOCATION_ONFIELD,0,nil,0x1e71)*-100
+function c38988538.vfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0x1e71)
+end
+function c38988538.val(e,c)
+	return Duel.GetMatchingGroupCount(c38988538.vfilter,e:GetOwnerPlayer(),LOCATION_ONFIELD,0,nil)*-100
 end
