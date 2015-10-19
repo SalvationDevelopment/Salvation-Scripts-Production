cbc4bb4b84c188277032efe1635e52f6f1f0baea
Ragna_Edge <j.yarel.g.aponte@gmail.com>
2015-10-19 19:29:54 -0400

    Fixed Everything

--- c63251695.lua
+++ c63251695.lua
@@ -9,6 +9,7 @@ function c63251695.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e1)
+	--Replace
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(63251695,0))
 	e2:SetCategory(CATEGORY_DISABLE)
@@ -16,13 +17,12 @@ function c63251695.initial_effect(c)
 	e2:SetProperty(EFFECT_FLAG_NO_TURN_RESET)
 	e2:SetCode(EVENT_CHAINING)
 	e2:SetRange(LOCATION_PZONE)
-	e2:SetCondition(c63251695.negcon)
-	e2:SetTarget(c63251695.negtg)
 	e2:SetOperation(c63251695.negop)
 	c:RegisterEffect(e2)
+	--Battle
 	local e3=Effect.CreateEffect(c)
 	e3:SetCategory(CATEGORY_TODECK)
-	e3:SetDescription(aux.Stringid(63251695,0))
+	e3:SetDescription(aux.Stringid(63251695,1))
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e3:SetCode(EVENT_DAMAGE_STEP_END)
 	e3:SetRange(LOCATION_MZONE)
@@ -31,25 +31,18 @@ function c63251695.initial_effect(c)
 	e3:SetOperation(c63251695.effop)
 	c:RegisterEffect(e3)
 end
-function c63251695.tfilter(c)
-	return c:IsFaceup() and c:IsSetCard(0x1e71) and c:IsControler(tp) and c:GetLocation()==LOCATION_ONFIELD
-end
-function c63251695.negcon(e,tp,eg,ep,ev,re,r,rp)
-	if not re:IsHasProperty(EFFECT_FLAG_CARD_TARGET) then return end
-	if not e:GetHandler():GetFlagEffect(63251695)==0 then return end
-	local g=Duel.GetChainInfo(ev,CHAININFO_TARGET_CARDS)
-	return g and g:IsExists(c63251695.tfilter,1,nil) and g:GetFirst()~=e:GetHandler() and Duel.IsChainDisablable(ev)
-end
-function c63251695.negtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
-	Duel.SetOperationInfo(0,CATEGORY_DISABLE,eg,1,0,0)
+function c63251695.tfilter(c,tp)
+	return c:IsLocation(LOCATION_ONFIELD) and c:IsControler(tp) and c:IsFaceup() and c:IsSetCard(0x1e71)
 end
 function c63251695.negop(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.SelectYesNo(tp,aux.Stringid(63251695,3)) then
-		e:GetHandler():RegisterFlagEffect(63251695,RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END,0,1)
+	if ep==tp then return end
+	if not re:IsHasProperty(EFFECT_FLAG_CARD_TARGET) then return end
+	local g=Duel.GetChainInfo(ev,CHAININFO_TARGET_CARDS)
+	if g and g:IsExists(c63251695.tfilter,1,nil,tp) and g~=e:GetHandler() and Duel.IsChainDisablable(ev) 
+		and Duel.SelectYesNo(tp,aux.Stringid(63251695,0))then
 		Duel.NegateEffect(ev)
 		Duel.Destroy(e:GetHandler(),REASON_EFFECT)
-	else end
+	end
 end
 function c63251695.effcon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler()==Duel.GetAttacker() 
@@ -63,7 +56,7 @@ function c63251695.efftg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE)==0 then
 		opt=Duel.SelectOption(tp,aux.Stringid(63251695,1))
 	else
-		opt=Duel.SelectOption(tp,aux.Stringid(63251695,0),aux.Stringid(63251695,1))
+		opt=Duel.SelectOption(tp,aux.Stringid(63251695,2),aux.Stringid(63251695,1))
 	end
 	e:SetLabel(opt)
 	if opt==0 then
@@ -72,9 +65,8 @@ function c63251695.efftg(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function c63251695.effop(e,tp,eg,ep,ev,re,r,rp)
 	if e:GetLabel()==1 then
-		local g1=Duel.GetMatchingGroup(Card.IsAbleToDeck,tp,0,LOCATION_HAND+LOCATION_ONFIELD,nil)
-		local sg1=g1:RandomSelect(tp,1)
-		local tc=sg1:GetFirst()
+		local g=Duel.SelectMatchingCard(tp,Card.IsAbleToDeck,tp,0,LOCATION_HAND+LOCATION_ONFIELD,1,1,nil)
+		local tc=g:GetFirst()
 		local c=e:GetHandler()
 		Duel.SendtoDeck(tc,nil,2,REASON_EFFECT)
 		if c:IsRelateToEffect(e) and c:IsFaceup() then
