711fd912e86a4e876913283326c276d5d3d90958
ChibiChanNya <chibi_theone@yahoo.com>
2015-10-19 23:11:48 -0500

    Exod WORKS

--- c13701812.lua
+++ c13701812.lua
@@ -20,7 +20,7 @@ function c13701812.initial_effect(c)
 	c:RegisterEffect(e2)
 	--tohand
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(93298460,0))
+	e3:SetDescription(aux.Stringid(13701812,0))
 	e3:SetCategory(CATEGORY_TOHAND)
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e3:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
@@ -31,26 +31,28 @@ function c13701812.initial_effect(c)
 	e3:SetOperation(c13701812.operation)
 	c:RegisterEffect(e3)
 end
+
 function c13701812.exodfilter(c)
 	return (c:IsCode(12600382) or c:IsCode(33244944) or c:IsCode(8124921) or c:IsCode(44519536) or 
 	c:IsCode(70903634) or c:IsCode(7902349) or c:IsCode(33396948) or c:IsCode(13701813)) and 
 	((c:IsLocation(LOCATION_DECK+LOCATION_HAND) and c:IsAbleToGrave()) or (c:IsLocation(LOCATION_GRAVE) and c:IsAbleToHand()))
 end
+
 function c13701812.target0(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
-	if Duel.GetFlagEffect(tp,13701812)==0 and Duel.IsExistingMatchingCard(c13701812.exodfilter,tp,LOCATION_DECK+LOCATION_HAND,0,1,nil) and 
-	Duel.IsExistingTarget(Card.IsAbleToHand,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) and
-	Duel.SelectYesNo(tp,aux.Stringid(13701812,0)) then 
+	if Duel.GetFlagEffect(tp,13701812)==0 
+	and Duel.IsExistingMatchingCard(c13701812.exodfilter,tp,LOCATION_DECK+LOCATION_HAND,0,1,nil) 
+	and Duel.IsExistingTarget(Card.IsAbleToHand,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil)
+	and	Duel.SelectYesNo(tp,aux.Stringid(13701812,0)) then 
+		e:SetCategory(CATEGORY_TOGRAVE+CATEGORY_TOHAND)
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
-		local g=Duel.SelectTarget(tp,Card.IsAbleToHand,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
 		Duel.RegisterFlagEffect(tp,13701812,RESET_PHASE+PHASE_END,0,1)
+		local g=Duel.SelectTarget(tp,Card.IsAbleToHand,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
 		Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK)
 		e:SetLabel(1)
-		e:GetHandler():RegisterFlagEffect(0,RESET_CHAIN,EFFECT_FLAG_CLIENT_HINT,1,0,aux.Stringid(13701812,2))
-	
+		e:GetHandler():RegisterFlagEffect(0,RESET_CHAIN,EFFECT_FLAG_CLIENT_HINT,1,0,aux.Stringid(13701812,2))	
 	else 
 		e:SetCategory(0)
-		e:SetProperty(0)
 		e:SetLabel(0)
 	end
 end
@@ -58,20 +60,20 @@ end
 function c13701812.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetFlagEffect(tp,13701812)==0 end
 	Duel.RegisterFlagEffect(tp,13701812,RESET_PHASE+PHASE_END,0,1)
+end
 	
 	
 function c13701812.target1(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetFlagEffect(tp,13701812)==0 and Duel.IsExistingMatchingCard(c13701812.exodfilter,tp,LOCATION_DECK+LOCATION_HAND,0,1,nil) and 
-	Duel.IsExistingTarget(Card.IsAbleToHand,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end 
+	if chk==0 then return Duel.IsExistingMatchingCard(c13701812.exodfilter,tp,LOCATION_DECK+LOCATION_HAND,0,1,nil) 
+	and Duel.IsExistingTarget(Card.IsAbleToHand,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end 
 	e:SetCountLimit(1,13701812)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
 	local g=Duel.SelectTarget(tp,Card.IsAbleToHand,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
-	Duel.RegisterFlagEffect(tp,13701812,RESET_PHASE+PHASE_END,0,1)
 	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK)
 end
 
 function c13701812.operation1(e,tp,eg,ep,ev,re,r,rp)
-	if not e:GetHandler():IsRelateToEffect(e) then return end
+	if e:GetLabel()==0 or not e:GetHandler():IsRelateToEffect(e) then return end
 	local tc=Duel.GetFirstTarget()
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
 	local g=Duel.SelectMatchingCard(tp,c13701812.exodfilter,tp,LOCATION_DECK+LOCATION_HAND,0,1,1,nil)
@@ -84,8 +86,9 @@ function c13701812.operation1(e,tp,eg,ep,ev,re,r,rp)
 end
 
 function c13701812.condition(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():IsPreviousLocation(LOCATION_ONFIELD)
+	return e:GetHandler():IsPreviousLocation(LOCATION_ONFIELD) and e:GetLabel()==1
 end
+
 function c13701812.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and exodfilter(chkc) end
 	if chk==0 then return Duel.IsExistingTarget(c13701812.exodfilter,tp,LOCATION_GRAVE,0,1,nil) end
