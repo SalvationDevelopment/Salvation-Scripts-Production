fd253f051fdd9c40eef609555f9fbc78fc7c2851
Ragna_Edge <j.yarel.g.aponte@gmail.com>
2015-10-22 18:42:47 -0400

    Updated Immune effect.

--- c41554273.lua
+++ c41554273.lua
@@ -1,4 +1,5 @@
 --Dinomist Rush
+--Fixed by Ragna_Edge
 function c41554273.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
@@ -23,8 +24,8 @@ function c41554273.activate(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectMatchingCard(tp,c41554273.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
 	local tc=g:GetFirst()
-	if Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)~=0 then
-		local e1=Effect.CreateEffect(tc)
+	if tc and Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP) then
+			local e1=Effect.CreateEffect(c)
 			e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 			e1:SetCode(EVENT_PHASE+PHASE_END)
 			e1:SetCountLimit(1)
@@ -34,7 +35,7 @@ function c41554273.activate(e,tp,eg,ep,ev,re,r,rp)
 			e1:SetCondition(c41554273.descon)
 			Duel.RegisterEffect(e1,tp)
 			tc:RegisterFlagEffect(41554273,RESET_EVENT+0x1fe0000,0,1)
-			local e2=Effect.CreateEffect(tc)
+			local e2=Effect.CreateEffect(c)
 			e2:SetType(EFFECT_TYPE_SINGLE)
 			e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 			e2:SetRange(LOCATION_MZONE)
@@ -42,6 +43,7 @@ function c41554273.activate(e,tp,eg,ep,ev,re,r,rp)
 			e2:SetValue(c41554273.efilter)
 			e2:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+RESET_END)
 			tc:RegisterEffect(e2)
+			Duel.SpecialSummonComplete()
 	end
 end
 function c41554273.descon(e,tp,eg,ep,ev,re,r,rp)
