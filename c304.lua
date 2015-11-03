7d335c02b59dc82267ad45acf5bac55221cd91ae
StormWulf <auronzeroo@aim.com>
2015-11-03 18:17:34 -0500

    Update Star Wolf (Z-CWA)

--- c304.lua
+++ c304.lua
@@ -9,6 +9,16 @@ function c304.initial_effect(c)
 	e1:SetTarget(c304.target)
 	e1:SetOperation(c304.operation)
 	c:RegisterEffect(e1)
+	--special summon
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCountLimit(1,305)
+	e2:SetCost(c304.spcost)
+	e2:SetTarget(c304.sptg)
+	e2:SetOperation(c304.spop)
+	c:RegisterEffect(e2)
 end
 
 function c304.filter(c)
@@ -67,4 +77,25 @@ function c304.operation(e,tp,eg,ep,ev,re,r,rp)
 end
 function c304.splimit(e,c)
 	return c:GetRace()~=RACE_BEAST
+end
+function c304.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToGraveAsCost,tp,LOCATION_HAND,0,1,nil) end
+	Duel.DiscardHand(tp,Card.IsAbleToGraveAsCost,1,1,REASON_COST)
+end
+function c304.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function c304.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)~=0 then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetReset(RESET_EVENT+0x47e0000)
+		e1:SetValue(LOCATION_REMOVED)
+		c:RegisterEffect(e1,true)
+	end
 end
