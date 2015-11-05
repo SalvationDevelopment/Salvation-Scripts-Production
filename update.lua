23b9e26b0746f68741241d677d5d708a17046fef
StormWulf <auronzeroo@aim.com>
2015-11-04 20:53:07 -0500

    Make sure the DB updates with Burgers

--- update.lua
+++ update.lua
@@ -17,3 +17,11 @@ srgopijsdofsdposdafsdfosdfpoisdpoijsposdfposdfopsdafadosifjdsafoijsdafp[kdfsa[pk
 George Bush did 9/11
 
 srgopijsdofsdposdafsdfosdfpoisdpoijsposdfposdfopsdafadosifjdsafoijsdafp[kdfsa[pksdf[pkf[pdsf[psdf[pkfdsk[p
+
+George Bush did 9/11
+
+srgopijsdofsdposdafsdfosdfpoisdpoijsposdfposdfopsdafadosifjdsafoijsdafp[kdfsa[pksdf[pkf[pdsf[psdf[pkfdsk[p
+
+George Bush did 9/11
+
+srgopijsdofsdposdafsdfosdfpoisdpoijsposdfposdfopsdafadosifjdsafoijsdafp[kdfsa[pksdf[pkf[pdsf[psdf[pkfdsk[p
