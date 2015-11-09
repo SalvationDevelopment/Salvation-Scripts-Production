b7e0d9a9f17356fded139c07686108527fd54b6c
StormWulf <auronzeroo@aim.com>
2015-11-09 10:55:11 -0500

    Make Priest with Blue Eyes recognized by updater

--- update.lua
+++ update.lua
@@ -1,9 +1,3 @@
-
-
-srgopijsdofsdposdafsdfosdfpoisdpoijsposdfposdfopsdafadosifjdsafoijsdafp[kdfsa[pksdf[pkf[pdsf[psdf[pkfdsk[p
-
-George Bush did 9/11
-
 srgopijsdofsdposdafsdfosdfpoisdpoijsposdfposdfopsdafadosifjdsafoijsdafp[kdfsa[pksdf[pkf[pdsf[psdf[pkfdsk[p
 
 George Bush did 9/11
