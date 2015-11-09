e44152b9afb300d2a06d0aac9d286b4f55aba2be
StormWulf <auronzeroo@aim.com>
2015-11-08 21:40:42 -0500

    Confirm database update

--- update.lua
+++ update.lua
@@ -1,8 +1,4 @@
-George Bush did 9/11
 
-srgopijsdofsdposdafsdfosdfpoisdpoijsposdfposdfopsdafadosifjdsafoijsdafp[kdfsa[pksdf[pkf[pdsf[psdf[pkfdsk[p
-
-George Bush did 9/11
 
 srgopijsdofsdposdafsdfosdfpoisdpoijsposdfposdfopsdafadosifjdsafoijsdafp[kdfsa[pksdf[pkf[pdsf[psdf[pkfdsk[p
 
