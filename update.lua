576fdfc951166a2ff2ec6c88d8979dc88c38cca3
StormWulf <auronzeroo@aim.com>
2015-11-12 14:26:03 -0500

    Confirm updates

--- update.lua
+++ update.lua
@@ -1,28 +1,5 @@
 srgopijsdofsdposdafsdfosdfpoisdpoijsposdfposdfopsdafadosifjdsafoijsdafp[kdfsa[pksdf[pkf[pdsf[psdf[pkfdsk[p
 
-George Bush did 9/11
-
-srgopijsdofsdposdafsdfosdfpoisdpoijsposdfposdfopsdafadosifjdsafoijsdafp[kdfsa[pksdf[pkf[pdsf[psdf[pkfdsk[p
-
-George Bush did 9/11
-
-srgopijsdofsdposdafsdfosdfpoisdpoijsposdfposdfopsdafadosifjdsafoijsdafp[kdfsa[pksdf[pkf[pdsf[psdf[pkfdsk[p
-
-George Bush did 9/11
-
-srgopijsdofsdposdafsdfosdfpoisdpoijsposdfposdfopsdafadosifjdsafoijsdafp[kdfsa[pksdf[pkf[pdsf[psdf[pkfdsk[p
-
-George Bush did 9/11
-
-srgopijsdofsdposdafsdfosdfpoisdpoijsposdfposdfopsdafadosifjdsafoijsdafp[kdfsa[pksdf[pkf[pdsf[psdf[pkfdsk[p
-
-srgopijsdofsdposdafsdfosdfpoisdpoijsposdfposdfopsdafadosifjdsafoijsdafp[kdfsa[pksdf[pkf[pdsf[psdf[pkfdsk[p
-
-George Bush did 9/11
-
-srgopijsdofsdposdafsdfosdfpoisdpoijsposdfposdfopsdafadosifjdsafoijsdafp[kdfsa[pksdf[pkf[pdsf[psdf[pkfdsk[p
-
-George Bush did 9/11
 
 srgopijsdofsdposdafsdfosdfpoisdpoijsposdfposdfopsdafadosifjdsafoijsdafp[kdfsa[pksdf[pkf[pdsf[psdf[pkfdsk[p
 
