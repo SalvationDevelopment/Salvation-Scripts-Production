0c4b679814a1f7da3aaeb8833768975e1944b34f
HelixReactor <AmineBelaid2909@gmail.com>
2015-11-19 08:21:09 -0500

    Fixed Ra - God Phoenix

--- c13701820.lua
+++ c13701820.lua
@@ -52,7 +52,7 @@ function c13701820.initial_effect(c)
 
 end
 function c13701820.cfilter(c,tp)
-	return c:IsCode(10000010)
+	return c:IsCode(10000010) and c:IsControler(tp) and c:IsPreviousLocation(LOCATION_ONFIELD)
 end
 function c13701820.spcon(e,tp,eg,ep,ev,re,r,rp)
 	return eg:IsExists(c13701820.cfilter,1,nil,tp)
