0a7fdad66b4f6b73ac18ada6cb10fab153813997
StormWulf <auronzeroo@aim.com>
2015-11-25 17:44:06 -0500

    Took Dinomist scripts from FH to fix Pendulum effects
    Dinomist Stegosaurus
    Dinomist Ceratops
    Dinomist Pteran

--- c1580833.lua
+++ c1580833.lua
@@ -1,13 +1,8 @@
 --Dinomist Stegosaurus
---Fixed by Ragna_edge
+--FH version
 function c1580833.initial_effect(c)
 	--pendulum summon
-	aux.AddPendulumProcedure(c)
-	--Activate
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_FREE_CHAIN)
-	c:RegisterEffect(e1)
+	aux.EnablePendulumAttribute(c)
 	--destroy replace
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
@@ -17,18 +12,21 @@ function c1580833.initial_effect(c)
 	e2:SetValue(c1580833.repval)
 	e2:SetOperation(c1580833.repop)
 	c:RegisterEffect(e2)
+	--destroy
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(1580833,0))
+	e3:SetCategory(CATEGORY_DESTROY)
 	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e3:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e3:SetCode(EVENT_BATTLED)
 	e3:SetRange(LOCATION_MZONE)
-	e3:SetCondition(c1580833.regcon)
-	e3:SetOperation(c1580833.regop)
+	e3:SetCountLimit(1)
+	e3:SetCondition(c1580833.descon)
+	e3:SetTarget(c1580833.destg)
+	e3:SetOperation(c1580833.desop)
 	c:RegisterEffect(e3)
 end
 function c1580833.filter(c,tp)
-	return c:IsFaceup() and c:IsLocation(LOCATION_ONFIELD) and c:IsControler(tp) and c:IsSetCard(0x1e71)
-	and not c:IsReason(REASON_REPLACE)
+	return c:IsFaceup() and c:IsControler(tp) and c:IsLocation(LOCATION_ONFIELD) and c:IsSetCard(0xd8)
+		and (c:IsReason(REASON_BATTLE) or (c:IsReason(REASON_EFFECT) and c:GetReasonPlayer()~=tp))
 end
 function c1580833.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return eg:IsExists(c1580833.filter,1,e:GetHandler(),tp) and not e:GetHandler():IsStatus(STATUS_DESTROY_CONFIRMED) end
@@ -40,24 +38,24 @@ end
 function c1580833.repop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Destroy(e:GetHandler(),REASON_EFFECT+REASON_REPLACE)
 end
-
-function c1580833.regcon(e,tp,eg,ep,ev,re,r,rp)
-	return (e:GetHandler()~=Duel.GetAttacker() and Duel.GetAttackTarget()~=nil and e:GetHandler()~=Duel.GetAttackTarget())
-	and ((Duel.GetAttacker():IsType(TYPE_PENDULUM) and Duel.GetAttacker():GetControler()==tp) or 
-	(Duel.GetAttackTarget():IsType(TYPE_PENDULUM) and Duel.GetAttackTarget():GetControler()==tp))
+function c1580833.descon(e,tp,eg,ep,ev,re,r,rp)
+	local a=Duel.GetAttacker()
+	local d=Duel.GetAttackTarget()
+	if not d then return false end
+	if d:IsControler(tp) then a,d=d,a end
+	return a:IsType(TYPE_PENDULUM) and a~=e:GetHandler() and d:IsControler(1-tp)
 end
-function c1580833.regop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if c:IsRelateToEffect(e) then
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		e1:SetCode(EVENT_BATTLED)
-		e1:SetOperation(c1580833.desop)
-		e1:SetReset(RESET_PHASE+PHASE_DAMAGE)
-		Duel.RegisterEffect(e1,tp)
-	end
+function c1580833.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local a=Duel.GetAttacker()
+	local d=Duel.GetAttackTarget()
+	if chk==0 then return a:IsDestructable() and d:IsDestructable() end
+	local g=Group.FromCards(a,d)
+	Duel.SetTargetCard(g)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,2,0,0)
 end
 function c1580833.desop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Destroy(Duel.GetAttacker(),REASON_EFFECT)
-	Duel.Destroy(Duel.GetAttackTarget(),REASON_EFFECT)
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS):Filter(Card.IsRelateToEffect,nil,e)
+	if g:GetCount()>0 then
+		Duel.Destroy(g,REASON_EFFECT)
+	end
 end
