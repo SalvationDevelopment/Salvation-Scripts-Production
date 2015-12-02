0d647e4fe57ab2a05bee130818acab72afa48654
StormWulf <auronzeroo@aim.com>
2015-12-02 02:06:39 -0500

    Fixed ID for Magnaslayer

--- c47819246.lua
+++ c47819246.lua
@@ -1,5 +1,5 @@
 --Super Quantum Megasword - Magnaslayer
-function c13754009.initial_effect(c)
+function c47819246.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_EQUIP)
@@ -7,9 +7,9 @@ function c13754009.initial_effect(c)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetHintTiming(TIMING_DAMAGE_STEP)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP)
-	e1:SetCondition(c13754009.condition)
-	e1:SetTarget(c13754009.target)
-	e1:SetOperation(c13754009.operation)
+	e1:SetCondition(c47819246.condition)
+	e1:SetTarget(c47819246.target)
+	e1:SetOperation(c47819246.operation)
 	c:RegisterEffect(e1)
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_EQUIP)
@@ -20,35 +20,35 @@ function c13754009.initial_effect(c)
 	e3:SetType(EFFECT_TYPE_QUICK_O)
 	e3:SetCode(EVENT_FREE_CHAIN)
 	e3:SetRange(LOCATION_SZONE)
-	e3:SetCondition(c13754009.atcon)
-	e3:SetCost(c13754009.atcost)
-	e3:SetTarget(c13754009.attg)
-	e3:SetOperation(c13754009.atop)
+	e3:SetCondition(c47819246.atcon)
+	e3:SetCost(c47819246.atcost)
+	e3:SetTarget(c47819246.attg)
+	e3:SetOperation(c47819246.atop)
 	c:RegisterEffect(e3)
 	--Atk up
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_EQUIP)
 	e2:SetCode(EFFECT_UPDATE_ATTACK)
-	e2:SetValue(c13754009.atkval)
+	e2:SetValue(c47819246.atkval)
 	c:RegisterEffect(e2)
 end
-function c13754009.atkval(e,c)
+function c47819246.atkval(e,c)
 	return c:GetRank()*100
 end
-function c13754009.condition(e,tp,eg,ep,ev,re,r,rp)
+function c47819246.condition(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetCurrentPhase()~=PHASE_DAMAGE or not Duel.IsDamageCalculated()
 end
-function c13754009.filter(c)
+function c47819246.filter(c)
 	return c:IsFaceup() and c:IsType(TYPE_XYZ) and c:IsSetCard(0xd5)
 end
-function c13754009.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and c13754009.filter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(c13754009.filter,tp,LOCATION_MZONE,0,1,nil) end
+function c47819246.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and c47819246.filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c47819246.filter,tp,LOCATION_MZONE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
-	local g=Duel.SelectTarget(tp,c13754009.filter,tp,LOCATION_MZONE,0,1,1,nil)
+	local g=Duel.SelectTarget(tp,c47819246.filter,tp,LOCATION_MZONE,0,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_EQUIP,e:GetHandler(),1,0,0)
 end
-function c13754009.operation(e,tp,eg,ep,ev,re,r,rp)
+function c47819246.operation(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if not c:IsLocation(LOCATION_SZONE) then return end
 	local tc=Duel.GetFirstTarget()
@@ -66,20 +66,20 @@ function c13754009.operation(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 
-function c13754009.atcon(e,tp,eg,ep,ev,re,r,rp)
+function c47819246.atcon(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetTurnPlayer()==tp and Duel.GetCurrentPhase()==PHASE_BATTLE and Duel.GetCurrentChain()==0
 end
-function c13754009.atcost(e,tp,eg,ep,ev,re,r,rp,chk)
+function c47819246.atcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsAbleToGraveAsCost() end
 	e:SetLabelObject(e:GetHandler():GetEquipTarget())
 	Duel.SendtoGrave(e:GetHandler(),REASON_COST)
 end
-function c13754009.attg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c47819246.attg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local eqc=e:GetHandler():GetEquipTarget()
 	if chk==0 then return eqc end
 	Duel.SetTargetCard(e:GetLabelObject())
 end
-function c13754009.atop(e,tp,eg,ep,ev,re,r,rp)
+function c47819246.atop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
 	if tc:IsFaceup() and tc:IsRelateToEffect(e) then
