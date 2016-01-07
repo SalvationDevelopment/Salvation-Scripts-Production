e71394c966f907b26d2cc9bde74b8c5c5b75f6e8
OneShot119 <dlang119@gmail.com>
2016-01-06 21:54:42 -0500

    Rector Pendulum

---
+++ c700908023.lua
@@ -0,0 +1,42 @@
+--Rector Pendulum, the Dracoverlord
+function c700908023.initial_effect(c)
+	--destroy
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(72001832,0))
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e1:SetCode(EVENT_BATTLE_START)
+	e1:SetTarget(c700908023.destg)
+	e1:SetOperation(c700908023.desop)
+	c:RegisterEffect(e1)
+	--disable
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetRange(LOCATION_PZONE)
+	e2:SetTargetRange(0,LOCATION_MZONE)
+	e2:SetTarget(c700908023.disable)
+	e2:SetCode(EFFECT_DISABLE)
+	c:RegisterEffect(e2)
+end
+function c700908023.disable(e,c)
+	return c:IsType(TYPE_PENDULUM)
+end
+function c700908023.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	local tc=Duel.GetAttacker()
+	if tc==c then tc=Duel.GetAttackTarget() end
+	if chk==0 then return tc and tc:IsFaceup() and tc:IsType(TYPE_PENDULUM) end
+	local g=Group.CreateGroup()
+	g:AddCard(c)
+	g:AddCard(tc)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+end
+function c700908023.desop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetAttacker()
+	if tc==c then tc=Duel.GetAttackTarget() end
+	local g=Group.CreateGroup()
+	g:AddCard(c)
+	g:AddCard(tc)
+	if tc:IsRelateToBattle() then Duel.Destroy(g,REASON_EFFECT) end
+end
+
