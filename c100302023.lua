fa2d57f4accdbb05bdbae948f06d32f8e0901d02
StormWulf <auronzeroo@aim.com>
2016-01-14 15:40:20 -0500

    New placeholder IDs

---
+++ c100302023.lua
@@ -0,0 +1,125 @@
+--巨神竜の遺跡
+--Ruins of the Great Divine Dragon
+function c100302023.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_DESTROY)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCondition(c100302023.condition)
+	e2:SetTarget(c100302023.target)
+	e2:SetOperation(c100302023.operation)
+	c:RegisterEffect(e2)
+	--To hand
+	local e3=Effect.CreateEffect(c)
+	e3:SetCategory(CATEGORY_TOKEN)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetCountLimit(1)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetCost(c100302023.tkcost)
+	e3:SetTarget(c100302023.tktg)
+	e3:SetOperation(c100302023.tkop)
+	c:RegisterEffect(e3)
+	--to hand
+	local e4=Effect.CreateEffect(c)
+	e4:SetCategory(CATEGORY_TOHAND)
+	e4:SetType(EFFECT_TYPE_IGNITION)
+	e4:SetRange(LOCATION_GRAVE)
+	e4:SetCost(c100302023.cost)
+	e4:SetTarget(c100302023.tg)
+	e4:SetOperation(c100302023.op)
+	c:RegisterEffect(e4)
+end
+function c100302023.cfilter(c)
+	return c:IsFaceup() and not c:IsPreviousLocation(LOCATION_GRAVE)
+end
+function c100302023.dfilter(c)
+	return c:IsFaceup() and c:IsRace(RACE_DRAGON) and (c:GetLevel()==7 or c:GetLevel()==8)
+end
+function c100302023.condition(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(c100302023.cfilter,1,nil) and Duel.IsExistingMatchingCard(c100302023.dfilter,tp,LOCATION_MZONE,0,1,nil)
+end
+function c100302023.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	local g=eg:Filter(c100302023.cfilter,nil)
+	Duel.SetTargetCard(eg)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,g:GetCount(),0,0)
+end
+function c100302023.filter(c,e)
+	return c:IsFaceup() and not c:IsPreviousLocation(LOCATION_GRAVE) and c:IsRelateToEffect(e)
+end
+function c100302023.operation(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local g=eg:Filter(c100302023.filter,nil,e)
+	if g:GetCount()>0 then
+	local tc=g:GetFirst()
+		while tc do
+			local e1=Effect.CreateEffect(e:GetHandler())
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_DISABLE)
+			e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+RESET_END)
+			tc:RegisterEffect(e1)
+			local e2=Effect.CreateEffect(e:GetHandler())
+			e2:SetType(EFFECT_TYPE_SINGLE)
+			e2:SetCode(EFFECT_DISABLE_EFFECT)
+			e2:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+RESET_END)
+			tc:RegisterEffect(e2)
+			if tc:IsType(TYPE_TRAPMONSTER) then
+				local e3=Effect.CreateEffect(e:GetHandler())
+				e3:SetType(EFFECT_TYPE_SINGLE)
+				e3:SetCode(EFFECT_DISABLE_TRAPMONSTER)
+				e3:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+RESET_END)
+				tc:RegisterEffect(e3)
+			end
+			tc=g:GetNext()
+		end
+	end
+end
+
+function c100302023.costfilter1(c)
+	return c:IsAbleToGraveAsCost() and c:IsFaceup()
+end
+function c100302023.tkcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c100302023.costfilter1,tp,LOCATION_ONFIELD,0,1,e:GetHandler()) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,c100302023.costfilter1,tp,LOCATION_ONFIELD,0,1,1,e:GetHandler())
+	Duel.SendtoGrave(g,REASON_COST)
+end
+function c100302023.tktg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,100302123,0,0x4011,0,0,1,RACE_DRAGON,ATTRIBUTE_LIGHT) end
+	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,0,0)
+end
+function c100302023.tkop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	if not (e:GetHandler():IsRelateToEffect(e) and Duel.IsPlayerCanSpecialSummonMonster(tp,100302123,0,0x4011,0,0,1,RACE_DRAGON,ATTRIBUTE_LIGHT)) then return end
+	local token=Duel.CreateToken(tp,100302123)
+	Duel.SpecialSummon(token,0,tp,tp,false,false,POS_FACEUP)
+end
+
+function c100302023.costfilter(c)
+	return c:IsRace(RACE_DRAGON) and (c:GetLevel()==7 or c:GetLevel()==8)
+end
+function c100302023.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c100302023.costfilter,tp,LOCATION_ONFIELD+LOCATION_HAND,0,1,e:GetHandler()) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,c100302023.costfilter,tp,LOCATION_ONFIELD+LOCATION_HAND,0,1,1,e:GetHandler())
+	Duel.SendtoGrave(g,REASON_COST)
+end
+function c100302023.tg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsAbleToHand() end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,e:GetHandler(),1,0,0)
+end
+function c100302023.op(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetHandler():IsRelateToEffect(e) then
+		Duel.SendtoHand(e:GetHandler(),nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,e:GetHandler())
+	end
+end
+
