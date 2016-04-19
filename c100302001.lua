468092a15049d98f1d40401ea7075f27daa61c41
StormWulf <auronzeroo@aim.com>
2016-04-19 15:12:42 -0400

    Now I know my ABCs

--- c100302001.lua
+++ c100302001.lua
@@ -1,73 +1,125 @@
---巨神竜フェルグラント
---Felgrand, the Great Divine Dragon
+--A－アサルト・コア
+--A-Assault Core
+--Script by mercury233
 function c100302001.initial_effect(c)
+	--equip
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_REMOVE)
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
-	e1:SetCondition(c100302001.thcon)
-	e1:SetTarget(c100302001.thtg)
-	e1:SetOperation(c100302001.thop)
+	e1:SetDescription(aux.Stringid(100302001,0))
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCategory(CATEGORY_EQUIP)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTarget(c100302001.eqtg)
+	e1:SetOperation(c100302001.eqop)
 	c:RegisterEffect(e1)
+	--unequip
 	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(100302001,1))
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
-	e2:SetCode(EVENT_BATTLE_DESTROYING)
-	e2:SetCondition(aux.bdocon)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCondition(c100302001.uncon)
 	e2:SetTarget(c100302001.sptg)
 	e2:SetOperation(c100302001.spop)
 	c:RegisterEffect(e2)
+	--destroy sub
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_EQUIP)
+	e3:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+	e3:SetCode(EFFECT_DESTROY_SUBSTITUTE)
+	e3:SetCondition(c100302001.uncon)
+	e3:SetValue(c100302001.repval)
+	c:RegisterEffect(e3)
+	--immune
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_EQUIP)
+	e4:SetCode(EFFECT_IMMUNE_EFFECT)
+	e4:SetCondition(c100302001.uncon)
+	e4:SetValue(c100302001.efilter)
+	c:RegisterEffect(e4)
+	--to hand
+	local e5=Effect.CreateEffect(c)
+	e5:SetCategory(CATEGORY_TOHAND)
+	e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e5:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e5:SetCode(EVENT_TO_GRAVE)
+	e5:SetCondition(c100302001.thcon)
+	e5:SetTarget(c100302001.thtg)
+	e5:SetOperation(c100302001.thop)
+	c:RegisterEffect(e5)
+	--eqlimit
+	local e6=Effect.CreateEffect(c)
+	e6:SetType(EFFECT_TYPE_SINGLE)
+	e6:SetCode(EFFECT_EQUIP_LIMIT)
+	e6:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e6:SetValue(c100302001.eqlimit)
+	c:RegisterEffect(e6)
 end
-function c100302001.thcon(e,tp,eg,ep,ev,re,r,rp)
+function c100302001.uncon(e)
+	return e:GetHandler():IsStatus(STATUS_UNION)
+end
+function c100302001.filter(c)
+	return c:IsFaceup() and c:IsRace(RACE_MACHINE) and c:IsAttribute(ATTRIBUTE_LIGHT)
+end
+function c100302001.eqtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	local c=e:GetHandler()
-	return c:GetSummonType()==SUMMON_TYPE_SPECIAL and c:IsPreviousLocation(LOCATION_GRAVE)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and c100302001.filter(chkc) end
+	if chk==0 then return e:GetHandler():GetFlagEffect(100302001)==0 and Duel.GetLocationCount(tp,LOCATION_SZONE)>0
+		and Duel.IsExistingTarget(c100302001.filter,tp,LOCATION_MZONE,0,1,c) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
+	local g=Duel.SelectTarget(tp,c100302001.filter,tp,LOCATION_MZONE,0,1,1,c)
+	Duel.SetOperationInfo(0,CATEGORY_EQUIP,g,1,0,0)
+	c:RegisterFlagEffect(100302001,RESET_EVENT+0x7e0000+RESET_PHASE+PHASE_END,0,1)
 end
-function c100302001.thfilter(c)
-	return c:IsType(TYPE_MONSTER)
-end
-function c100302001.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE+LOCATION_GRAVE) and chkc:IsControler(1-tp) and c100302001.thfilter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(c100302001.thfilter,tp,0,LOCATION_MZONE+LOCATION_GRAVE,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local g=Duel.SelectTarget(tp,c100302001.thfilter,tp,0,LOCATION_MZONE+LOCATION_GRAVE,1,1,nil)
-	local tc=g:GetFirst()
-	if tc and tc:IsAbleToRemove() then
-		Duel.SetOperationInfo(0,CATEGORY_REMOVE,tc,1,0,0)
-	end
-end
-function c100302001.thop(e,tp,eg,ep,ev,re,r,rp)
+function c100302001.eqop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
-	local c=e:GetHandler()
-	local atk=0
-	if tc:IsType(TYPE_XYZ) then  atk=tc:GetRank() else atk=tc:GetLevel() end
-	if tc and tc:IsRelateToEffect(e) then
-		if Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)~=0 then
-			local e1=Effect.CreateEffect(e:GetHandler())
-			e1:SetType(EFFECT_TYPE_SINGLE)
-			e1:SetCode(EFFECT_UPDATE_ATTACK)
-			e1:SetValue(atk*100)
-			e1:SetReset(RESET_EVENT+0x1fe0000)
-			c:RegisterEffect(e1)
-		end
+	if not c:IsRelateToEffect(e) or c:IsFacedown() then return end
+	if not tc:IsRelateToEffect(e) or not c100302001.filter(tc) then
+		Duel.SendtoGrave(c,REASON_EFFECT)
+		return
 	end
+	if not Duel.Equip(tp,c,tc,false) then return end
+	c:SetStatus(STATUS_UNION,true)
 end
-
-function c100302001.filter(c,e,tp)
-	return c:IsRace(RACE_DRAGON) and  (c:GetLevel()==7 or c:GetLevel()==8) and c:GetCode()~=100302001 and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-end
-function c100302001.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and c100302001.filter(chkc,e,tp) end
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingTarget(c100302001.filter,tp,LOCATION_GRAVE,LOCATION_GRAVE,1,nil,e,tp) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectTarget(tp,c100302001.filter,tp,LOCATION_GRAVE,LOCATION_GRAVE,1,1,nil,e,tp)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+function c100302001.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:GetFlagEffect(100302001)==0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,true,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
+	c:RegisterFlagEffect(100302001,RESET_EVENT+0x7e0000+RESET_PHASE+PHASE_END,0,1)
 end
 function c100302001.spop(e,tp,eg,ep,ev,re,r,rp)
-	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) then
-		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.SpecialSummon(c,0,tp,tp,true,false,POS_FACEUP)
 	end
+end
+function c100302001.repval(e,re,r,rp)
+	return bit.band(r,REASON_BATTLE)~=0 or bit.band(r,REASON_EFFECT)~=0
+end
+function c100302001.efilter(e,te)
+	return te:GetOwnerPlayer()~=e:GetHandlerPlayer() and te:GetOwner()~=e:GetOwner()
+		and te:IsActiveType(TYPE_MONSTER)
+end
+function c100302001.thcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsPreviousLocation(LOCATION_ONFIELD)
+end
+function c100302001.thfilter(c)
+	return c:IsType(TYPE_UNION) and c:IsAbleToHand() and not c:IsHasEffect(EFFECT_NECRO_VALLEY)
+end
+function c100302001.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c100302001.thfilter,tp,LOCATION_GRAVE,0,1,e:GetHandler()) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE)
+end
+function c100302001.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,c100302001.thfilter,tp,LOCATION_GRAVE,0,1,1,e:GetHandler())
+	if g:GetCount()>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+function c100302001.eqlimit(e,c)
+	return c:IsFaceup() and c:IsRace(RACE_MACHINE) and c:IsAttribute(ATTRIBUTE_LIGHT)
 end
