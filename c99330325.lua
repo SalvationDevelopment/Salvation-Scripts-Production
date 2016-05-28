532ffc75927ffe84aa19af8e564027c196d39cc4
StormWulf <auronzeroo@aim.com>
2016-05-27 21:30:57 -0400

    FH update for forced attack
    Karakuri Soldier mdl 236 "Nisamu"
    Karakuri Ninja mdl 919 "Kuick"
    Amazoness Willpower
    Hypnosister
    Karakuri Komachi mdl 224 "Ninishi"
    Karakuri Merchant mdl 177 "Inashichi"
    All-Out Attacks
    Battle Mania
    Savage Colosseum
    Karakuri Bushi mdl 6318 "Muzanichiha"
    Berserk Gorilla
    Karakuri Ninja mdl 7749 "Nanashick"
    Karakuri Strategist mdl 248 "Nishipachi"
    Karakuri Watchdog mdl 313 "Saizan"
    Battlin' Boxer Cheat Commissioner
    Yang Zing Unleashed
    Diffusion Wave-Motion
    Karakuri Barrel mdl 96 "Shinkuro"
    Karakuri Ninja mdl 339 "Sazank"
    Interrupted Kaiju Slumber (OCG)

--- c99330325.lua
+++ c99330325.lua
@@ -22,16 +22,6 @@ function c99330325.initial_effect(c)
 	e2:SetOperation(c99330325.thop)
 	c:RegisterEffect(e2)
 end
-
-function c99330325.tmpfilter1(c,e,tp)
-	return c:IsSetCard(0xd3) and c:IsType(TYPE_MONSTER) 
-		and Duel.IsPlayerCanSpecialSummonMonster(tp,c:GetCode(),cc,0x21,c:GetAttack(),c:GetDefence(),c:GetLevel(),c:GetRace(),c:GetAttribute(),POS_FACEUP_ATTACK,tp)
-		and Duel.IsExistingMatchingCard(c99330325.tmpfilter2,tp,LOCATION_DECK,0,1,nil,e,tp,c:GetCode())
-end
-function c99330325.tmpfilter2(c,e,tp,cd)
-	return c:IsSetCard(0xd3) and c:IsType(TYPE_MONSTER) and not c:IsCode(cd)
-		and Duel.IsPlayerCanSpecialSummonMonster(tp,c:GetCode(),cc,0x21,c:GetAttack(),c:GetDefence(),c:GetLevel(),c:GetRace(),c:GetAttribute(),POS_FACEUP_ATTACK,1-tp)
-end
 function c99330325.filter1(c,e,tp)
 	return c:IsSetCard(0xd3) and c:IsType(TYPE_MONSTER) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 		and Duel.IsExistingMatchingCard(c99330325.filter2,tp,LOCATION_DECK,0,1,nil,e,tp,c:GetCode())
@@ -42,20 +32,18 @@ function c99330325.filter2(c,e,tp,cd)
 end
 function c99330325.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDestructable,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil)
-		and Duel.IsExistingMatchingCard(c99330325.tmpfilter1,tp,LOCATION_DECK,0,1,nil,e,tp) and not Duel.IsPlayerAffectedByEffect(tp,59822133) end
+		and Duel.IsExistingMatchingCard(c99330325.filter1,tp,LOCATION_DECK,0,1,nil,e,tp) end
 	local g=Duel.GetMatchingGroup(Card.IsDestructable,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,g:GetCount(),0,0)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,2,tp,LOCATION_DECK)
 end
 function c99330325.activate(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.IsPlayerAffectedByEffect(tp,59822133) then return end
 	local dg=Duel.GetMatchingGroup(Card.IsDestructable,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
-	local sg=Duel.GetMatchingGroup(c99330325.tmpfilter1,tp,LOCATION_DECK,0,nil,e,tp)
-	if Duel.Destroy(dg,REASON_EFFECT)~=0 and sg:GetCount()>0
+	if Duel.Destroy(dg,REASON_EFFECT)==0 then return end
+	local sg=Duel.GetMatchingGroup(c99330325.filter1,tp,LOCATION_DECK,0,nil,e,tp)
+	if sg:GetCount()>0
 		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.GetLocationCount(1-tp,LOCATION_MZONE)>0 then
 		Duel.BreakEffect()
-		local sg=Duel.GetMatchingGroup(c99330325.filter1,tp,LOCATION_DECK,0,nil,e,tp)
-		if sg:GetCount()==0 then return end
 		Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(99330325,2))
 		local g1=sg:Select(tp,1,1,nil)
 		local tc1=g1:GetFirst()
@@ -76,22 +64,8 @@ function c99330325.activate(e,tp,eg,ep,ev,re,r,rp)
 		tc1:RegisterEffect(e3)
 		local e4=e3:Clone()
 		tc2:RegisterEffect(e4)
-		local e5=Effect.CreateEffect(e:GetHandler())
-		e5:SetType(EFFECT_TYPE_FIELD)
-		e5:SetCode(EFFECT_CANNOT_EP)
-		e5:SetRange(LOCATION_MZONE)
-		e5:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-		e5:SetTargetRange(1,0)
-		e5:SetCondition(c99330325.becon)
-		e5:SetReset(RESET_EVENT+0x1fe0000)
-		tc1:RegisterEffect(e5)
-		local e6=e5:Clone()
-		tc2:RegisterEffect(e6)
 		Duel.SpecialSummonComplete()
 	end
-end
-function c99330325.becon(e)
-	return e:GetHandler():IsAttackable()
 end
 function c99330325.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsAbleToRemoveAsCost() end
