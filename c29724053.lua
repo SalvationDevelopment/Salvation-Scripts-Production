36ebcc4de51e70bc15bf49cc4c8312fe580cf986
StormWulf <auronzeroo@aim.com>
2016-07-24 02:01:31 -0400

    FH fixes
    Change ID for Galaxy-Eyes Cipher Blade Dragon
    fix Summon Gate
    Change ID for Destiny HERO - Deadlyguy
    Change ID for Abyss Actor - Dandy Supporting Actor
    Fix for Sword Hunter
    Fix for Tellarknight Vatlamyus
    Change ID for Red Rising Dragon

--- c29724053.lua
+++ c29724053.lua
@@ -1,7 +1,4 @@
 --サモン・ゲート
---Summon Gate
---Script by mercury233
---Not fully implemented, pendulum summon don't limit the count
 function c29724053.initial_effect(c)
 	--activate
 	local e1=Effect.CreateEffect(c)
@@ -17,36 +14,35 @@ function c29724053.initial_effect(c)
 	e2:SetTargetRange(1,1)
 	e2:SetTarget(c29724053.sumlimit)
 	c:RegisterEffect(e2)
-	--counter
-	if c29724053.counter==nil then
-		c29724053.counter=true
-		c29724053[0]=0
-		c29724053[1]=0
+	if c29724053.global_check==nil then
+		c29724053.global_check=true
+		c29724053[0]=3
+		c29724053[1]=3
 		local ge1=Effect.CreateEffect(c)
 		ge1:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
 		ge1:SetCode(EVENT_PHASE_START+PHASE_DRAW)
-		ge1:SetOperation(c29724053.resetcount)
+		ge1:SetOperation(c29724053.resetop)
 		Duel.RegisterEffect(ge1,0)
 		local ge2=Effect.CreateEffect(c)
 		ge2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
 		ge2:SetCode(EVENT_SPSUMMON_SUCCESS)
-		ge2:SetOperation(c29724053.addcount)
+		ge2:SetOperation(c29724053.checkop)
 		Duel.RegisterEffect(ge2,0)
 	end
 end
 function c29724053.sumlimit(e,c,sump,sumtype,sumpos,targetp,se)
-	return c:IsLocation(LOCATION_EXTRA) and c29724053[sump]>=3
+	return c:IsLocation(LOCATION_EXTRA) and c29724053[sump]<=0
 end
-function c29724053.resetcount(e,tp,eg,ep,ev,re,r,rp)
-	c29724053[0]=0
-	c29724053[1]=0
+function c29724053.resetop(e,tp,eg,ep,ev,re,r,rp)
+	c29724053[0]=3
+	c29724053[1]=3
 end
-function c29724053.addcount(e,tp,eg,ep,ev,re,r,rp)
+function c29724053.checkop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=eg:GetFirst()
 	while tc do
-		if tc:GetPreviousLocation()==LOCATION_EXTRA then
-			local p=tc:GetReasonPlayer()
-			c29724053[p]=c29724053[p]+1
+		if tc:IsPreviousLocation(LOCATION_EXTRA) then
+			local p=tc:GetSummonPlayer()
+			c29724053[p]=c29724053[p]-1
 		end
 		tc=eg:GetNext()
 	end
