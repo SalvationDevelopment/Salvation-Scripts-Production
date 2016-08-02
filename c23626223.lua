2cc0a4102857f836228754d6a5a4230393ec30a2
StormWulf <auronzeroo@aim.com>
2016-08-01 21:49:04 -0400

    Trap Monster fixes
    Statue of Anguish Pattern
    First-Aid Squad
    Paleozoic Olenoides

--- c23626223.lua
+++ c23626223.lua
@@ -19,9 +19,9 @@ function c23626223.activate(e,tp,eg,ep,ev,re,r,rp)
 	if not c:IsRelateToEffect(e) then return end
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0
 		or not Duel.IsPlayerCanSpecialSummonMonster(tp,23626223,0,0x21,0,2500,7,RACE_ROCK,ATTRIBUTE_EARTH) then return end
-	c:AddMonsterAttribute(TYPE_EFFECT+TYPE_TRAP)
+	c:AddMonsterAttribute(0,0,0,0,0)
 	Duel.SpecialSummonStep(c,0,tp,tp,true,false,POS_FACEUP)
-	c:AddMonsterAttributeComplete()
+	c:TrapMonsterComplete(TYPE_EFFECT)
 	--monster effects
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
