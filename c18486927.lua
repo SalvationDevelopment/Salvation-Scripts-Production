a452609e525619555a0ceb36ade1be42872832df
StormWulf <auronzeroo@aim.com>
2016-09-24 21:49:17 -0400

    FH fixes
    "17663375","Ancient Gear Wyvern"
    "18486927","Ancient Gear
    Gadget"
    "23171610","Limiter Removal"
    "44052074","Ancient Gear
    Catapult"
    "44874522","Ancient Gear Reactor Dragon"
    "70147689","Ancient
    Gear Fortress"
    "81269231","Ancient Gear Hydra"
    "92001300","Ancient Gear
    Castle"

--- c18486927.lua
+++ c18486927.lua
@@ -1,5 +1,4 @@
 --古代の歯車機械
---Name change is temporary, needs a proper announce function
 function c18486927.initial_effect(c)
 	--declare card
 	local e1=Effect.CreateEffect(c)
@@ -25,11 +24,11 @@ end
 function c18486927.dectg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CARDTYPE)
-	Duel.SetTargetParam(Duel.SelectOption(tp,70,71,72))
+	e:SetLabel(Duel.SelectOption(tp,70,71,72))
 end
 function c18486927.decop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	local opt=Duel.GetChainInfo(0,CHAININFO_TARGET_PARAM)
+	local opt=e:GetLabel()
 	local ct=nil
 	if opt==0 then
 		ct=TYPE_MONSTER
@@ -52,8 +51,8 @@ function c18486927.decop(e,tp,eg,ep,ev,re,r,rp)
 end
 function c18486927.actlimit(e,re,tp)
 	local ct=e:GetLabel()
-	return re:IsActiveType(ct) and not re:GetHandler():IsImmuneToEffect(e)
-		and (re:IsHasType(EFFECT_TYPE_ACTIVATE) or re:IsActiveType(TYPE_MONSTER))
+	return re:IsActiveType(ct) and (ct==TYPE_MONSTER or re:IsHasType(EFFECT_TYPE_ACTIVATE))
+		and not re:GetHandler():IsImmuneToEffect(e)
 end
 function c18486927.actcon(e)
 	local tc=Duel.GetAttacker()
@@ -62,20 +61,14 @@ function c18486927.actcon(e)
 end
 function c18486927.nametg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
-	--Gadget Order: Green, Red, Yellow, Gold, Silver, Soldier, Hauler, Arms, Driver
-	local nt={aux.Stringid(18486927,1),aux.Stringid(18486927,2),aux.Stringid(18486927,3),aux.Stringid(18486927,4),aux.Stringid(18486927,5),
-		aux.Stringid(18486927,6),aux.Stringid(18486927,7),aux.Stringid(18486927,8),aux.Stringid(18486927,9)}
-	local ct={41172955,86445415,13839120,55010259,29021114,86281779,28002611,47985614,54497620}
-	Duel.Hint(HINT_SELECTMSG,tp,564)
-	local opt=Duel.SelectOption(tp,nt[1],nt[2],nt[3],nt[4],nt[5],nt[6],nt[7],nt[8],nt[9])+1
-	local ac=ct[opt]
+	local ac=Duel.AnnounceCardFilter(tp,0x51,OPCODE_ISSETCARD,18486927,OPCODE_ISCODE,OPCODE_NOT,OPCODE_AND)
 	Duel.SetTargetParam(ac)
 	Duel.SetOperationInfo(0,CATEGORY_ANNOUNCE,nil,0,tp,ANNOUNCE_CARD)
 end
 function c18486927.nameop(e,tp,eg,ep,ev,re,r,rp)
 	local ac=Duel.GetChainInfo(0,CHAININFO_TARGET_PARAM)
 	local c=e:GetHandler()
-	if c:IsRelateToEffect(e) then
+	if c:IsRelateToEffect(e) and c:IsFaceup() then
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
