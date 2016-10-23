40f64f0929495fc75066406ed8e668f577ba9a58
StormWulf <auronzeroo@aim.com>
2016-10-23 01:36:06 -0400

    FH fixes
    "Number 9: Dyson Sphere"
    "Raremetalfoes Bismagia"
    "Cloudian -
    Nimbusman"
    "Voltanis the Adjudicator"
    "Speedroid Passing Rider"
    "Mist
    Archfiend"
    "Azure-Eyes Silver Dragon"
    "Darklord Ixtab"
    "Number 54: Lion
    Heart"
    "Absolute King Back Jack"
    "Powersink Stone"
    "Graceful
    Charity"
    "Soul of Silvermountain"
    "Archfiend Cavalry"
    "Brotherhood of
    the Fire Fist - Caribou"
    "Evil HERO Infernal Gainer"

--- c26420373.lua
+++ c26420373.lua
@@ -1,6 +1,4 @@
 --SRパッシングライダー
---Speedroid Passing Rider
---Script by mercury233
 function c26420373.initial_effect(c)
 	--pendulum summon
 	aux.EnablePendulumAttribute(c)
@@ -16,29 +14,29 @@ function c26420373.initial_effect(c)
 	--special summon
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
-	e2:SetCode(EFFECT_SPSUMMON_PROC)
 	e2:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e2:SetCode(EFFECT_SPSUMMON_PROC)
 	e2:SetRange(LOCATION_HAND)
 	e2:SetCountLimit(1,26420373)
-	e2:SetCondition(c26420373.spcon)
+	e2:SetCondition(c26420373.hspcon)
 	c:RegisterEffect(e2)
-	--summon success
+	--special summon
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(26420373,3))
 	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e3:SetCode(EVENT_SUMMON_SUCCESS)
 	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e3:SetCondition(c26420373.sumcon)
-	e3:SetTarget(c26420373.sumtg)
-	e3:SetOperation(c26420373.sumop)
+	e3:SetCode(EVENT_SUMMON_SUCCESS)
+	e3:SetCondition(c26420373.spcon)
+	e3:SetTarget(c26420373.sptg)
+	e3:SetOperation(c26420373.spop)
 	c:RegisterEffect(e3)
 	--cannot select battle target
 	local e4=Effect.CreateEffect(c)
 	e4:SetType(EFFECT_TYPE_FIELD)
+	e4:SetCode(EFFECT_CANNOT_SELECT_BATTLE_TARGET)
 	e4:SetRange(LOCATION_MZONE)
 	e4:SetTargetRange(0,LOCATION_MZONE)
-	e4:SetCode(EFFECT_CANNOT_SELECT_BATTLE_TARGET)
 	e4:SetValue(c26420373.atlimit)
 	c:RegisterEffect(e4)
 end
@@ -73,26 +71,26 @@ function c26420373.scop(e,tp,eg,ep,ev,re,r,rp)
 	e2:SetCode(EFFECT_UPDATE_RSCALE)
 	c:RegisterEffect(e2)
 end
-function c26420373.spcon(e,c)
+function c26420373.hspcon(e,c)
 	if c==nil then return true end
 	return Duel.GetFieldGroupCount(c:GetControler(),LOCATION_MZONE,LOCATION_MZONE)==0
 		and Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0
 end
-function c26420373.sumcon(e,tp,eg,ep,ev,re,r,rp)
+function c26420373.spcon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():GetSummonType()==SUMMON_TYPE_ADVANCE
 end
-function c26420373.filter(c,e,tp)
+function c26420373.spfilter(c,e,tp)
 	return c:IsSetCard(0x2016) and c:IsLevelBelow(4) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function c26420373.sumtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c26420373.filter(chkc,e,tp) end
-	if chk==0 then return Duel.IsExistingTarget(c26420373.filter,tp,LOCATION_GRAVE,0,1,nil,e,tp)
+function c26420373.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c26420373.spfilter(chkc,e,tp) end
+	if chk==0 then return Duel.IsExistingTarget(c26420373.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp)
 		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectTarget(tp,c26420373.filter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	local g=Duel.SelectTarget(tp,c26420373.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
 end
-function c26420373.sumop(e,tp,eg,ep,ev,re,r,rp)
+function c26420373.spop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) then
 		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
