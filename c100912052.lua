87a763ffe3b9d7c6c31a1fe5c603b7973a03620f
StormWulf <auronzeroo@aim.com>
2016-11-18 23:34:39 -0500

    Small fix for Transcensial Fusion

--- c100912052.lua
+++ c100912052.lua
@@ -5,7 +5,7 @@ function c100912052.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(100912052,0))
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCost(c100912052.cost)
